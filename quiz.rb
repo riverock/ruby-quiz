f06853cb93bdfe17b5fa5e034ad72261e8769584
Keifer Furzland <kfrz.code@gmail.com>
2016-06-03 19:13:04 -0500

    refactor to gather indices of dictionary words

--- quiz.rb
+++ quiz.rb
@@ -2,28 +2,34 @@ class Quiz
   attr_accessor :questions, :answers
 
   # TODO takes a file as input, outputs two files.
-  def initialize()
+  def initialize(file = nil)
     @questions = []
     @answers = []
   end
 
   def extract_unique(dictionary, word_length)
     uniques = Hash.new(0)
+    answers = Hash.new(0) 
 
     dictionary.each do |word|
       next if word.size < word_length
       word.chars.each_cons(word_length) do |char_group|
-        uniques[char_group.join] += 1 
+        uniques[char_group.join] += 1
+        answers[char_group.join] = dictionary.index(word) 
       end
     end
 
-    # ultimately I want a hash, uniques{}, that has keys of uniq char groups and values are array index of
-    # the original word from dictionary[]
-    #
-    # is it maybe more efficient to map dictionary to a hash and do a merge of some sort?
+    repeats = uniques.reject{ |v,k|k==1 }.keys
 
-    @questions = uniques.select{ |k,v| v == 1 }.keys # TODO write this to file
-    @answers = uniques.select{ |k,v| v == 1 }.values # TODO Map unique groups to original word in dictionary
+    repeats.each do |r|
+      uniques.delete(r)
+      answers.delete(r)
+    end
 
-  end 
+    @questions = uniques.keys # TODO write this to file
+
+    answers.values.each do |v|
+      @answers << dictionary[v]
+    end
+  end
 end
