32159d6702735149bfcf358f48f4b2e78f040e18
Riveascore <ChrisAga.dev@gmail.com>
2016-06-04 22:02:11 -0500

    question_and_answer_arrays implementation

--- questions_and_answers.rb
+++ questions_and_answers.rb
@@ -4,6 +4,7 @@ module QuestionsAndAnswers
   SEQUENCE_LENGTH = 4
   SEQUENCE_REGEX = Regexp.new("(?=([a-z0-9]{#{SEQUENCE_LENGTH}}))")
 
+  # TODO:
   def sequence_in_word?(sequence, word)
     #code
   end
@@ -12,16 +13,39 @@ module QuestionsAndAnswers
     return word.scan(SEQUENCE_REGEX)
   end
 
+  # TODO:
   def sequence_in_any_words?(sequence, word_to_ommit)
     #code
   end
 
+  # TODO:
   def generate_questions_and_answers
     #code
   end
 
+  # TODO:
   def write_to_file(filename, array)
     #code
+  end
+
+  def question_and_answer_arrays
+    questions_intial = []
+    answers_initial = []
+
+    words.each do |word|
+      sequences = sequences_from_word(word)
+
+      questions_intial.push(sequences)
+      answers_initial.push(sequences.size)
+    end
+
+    questions_initial.flatten.map do |question|
+      if question
+
+      else
+
+      end
+    end
   end
 
 end
