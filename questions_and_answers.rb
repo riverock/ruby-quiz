b3c274af7d4d2db88f50fc689926267821a97fe4
Riveascore <ChrisAga.dev@gmail.com>
2016-06-04 21:30:50 -0500

    Create questions_and_answers module
    Also add method and constant definitions

---
+++ questions_and_answers.rb
@@ -0,0 +1,26 @@
+module QuestionsAndAnswers
+
+  SEQUENCE_LENGTH = 4
+  WORDS_ARRAY = []
+
+  def sequence_in_word?(sequence, word)
+    #code
+  end
+
+  def sequences_from_word(word)
+    #code
+  end
+
+  def sequence_in_any_words?(sequence, word_to_ommit)
+    #code
+  end
+
+  def generate_questions_and_answers
+    #code
+  end
+
+  def write_to_file(filename, array)
+    #code
+  end
+
+end
