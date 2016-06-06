eeb8a774690c5aeaf7921eb9e9a571e6612dd9de
Riveascore <ChrisAga.dev@gmail.com>
2016-06-05 21:51:10 -0500

    Fix questions_and_answers tests

--- spec/questions_and_answers_spec.rb
+++ spec/questions_and_answers_spec.rb
@@ -12,23 +12,23 @@ describe '#sequences_from_word' do
   end
 end
 
-# describe "#get_words_array" do
-#   it "should return the correct array" do
-#     expect get_words_array('basic_example/words') == %w(arrows carrots give me)
-#   end
-# end
-#
-# describe "#questions_and_answers" do
-#   words_array = get_words_array('basic_example/words')
-#   questions, answers = questions_and_answers(words_array)
-#   questions_correct = get_words_array('basic_example/questions_correct')
-#   answers_correct = get_words_array('basic_example/answers_correct')
-#
-#   it "should return the correct questions" do
-#     expect(questions) == "hi"
-#   end
-#
-#   # it "should return the correct answers" do
-#   #   expect answers == answers_correct
-#   # end
-# end
+describe "#get_words_array" do
+  it "should return the correct array" do
+    expect(get_words_array('basic_example/words')).to eql %w(arrows carrots give me)
+  end
+end
+
+describe "#questions_and_answers" do
+  words_array = get_words_array('basic_example/words')
+  questions, answers = questions_and_answers(words_array).map { |x| x[:array] }
+  questions_correct = get_words_array('basic_example/questions_correct')
+  answers_correct = get_words_array('basic_example/answers_correct')
+
+  it "should return the correct questions" do
+    expect(questions).to eql questions_correct
+  end
+
+  it "should return the correct answers" do
+    expect(answers).to eql answers_correct
+  end
+end
