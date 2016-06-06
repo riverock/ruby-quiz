32a8339d7929ed52c16361a79e6895b357be0063
Riveascore <ChrisAga.dev@gmail.com>
2016-06-05 21:38:59 -0500

    Setup questions_and_answers test

--- spec/questions_and_answers_spec.rb
+++ spec/questions_and_answers_spec.rb
@@ -14,6 +14,21 @@ end
 
 describe "#get_words_array" do
   it "should return the correct array" do
-    expect get_words_array('words') == %w(arrows carrots give me)
+    expect get_words_array('basic_example/words') == %w(arrows carrots give me)
+  end
+end
+
+describe "#questions_and_answers" do
+  words_array = get_words_array('basic_example/words')
+  questions, answers = questions_and_answers(words_array)
+  questions_correct = get_words_array('basic_example/questions_correct')
+  answers_correct = get_words_array('basic_example/answers_correct')
+  
+  it "should return the correct questions" do
+    expect questions == questions_correct
+  end
+
+  it "should return the correct answers" do
+    expect answers == answers_correct
   end
 end
