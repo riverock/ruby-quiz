3a768b229df64ad02341ed5d76804afd78449a95
Riveascore <ChrisAga.dev@gmail.com>
2016-06-05 21:25:33 -0500

    Other sequences_from_word tests

--- questions_and_answers.rb
+++ questions_and_answers.rb
@@ -1,4 +1,4 @@
-module QuestionsAndAnswers
+  module QuestionsAndAnswers
 
   require 'pry'
   require 'pry-remote'
--- spec/questions_and_answers_spec.rb
+++ spec/questions_and_answers_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe '#sequences_from_word' do
-  it 'returns the correct sequences' do
+  it 'returns the correct sequences from arrows' do
     expect QuestionsAndAnswers.sequences_from_word('arrows') == %w(arro rrow rows)
+  end
+  it 'returns the correct sequences from carrots' do
+    expect QuestionsAndAnswers.sequences_from_word('carrots') == %w(carr arro rrot rots)
+  end
+  it 'returns the correct sequences from give' do
+    expect QuestionsAndAnswers.sequences_from_word('give') == %w(give)
   end
 end
